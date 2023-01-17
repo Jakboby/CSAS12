b7e86e2490fc8a2fcd325e646fc4359f29833bd5
Anthony Saldana Valle <55422282+ansavanix@users.noreply.github.com>
2023-01-17 11:17:17 -0800

    Add files via upload

---
+++ anthony.cpp
@@ -0,0 +1,9 @@
+bool getQuotedString(string& line, int &index, string& subString) {
+	subString = "";
+	index++;
+	while (line[index] != "\"") {
+		subString += line[index];
+		index++;
+	}
+	return index == line.size() - 1;
+}
